43d6c89d105483cec1f95e38f692efb0a1338aa7
Alexander Weld/leto3 <weldale@gmail.com>
2012-11-07 12:21:45 +0100

    lib-project scaffold random sql query

---
+++ sql/abfrage_kandidaten_eindeutig.sql
@@ -0,0 +1,11 @@
+﻿--Abfrage ob ein Kandidat mit gleichem Vorname,Nachname,Jahrgang,Partei doppelt vorkommt
+--Notwendig, weil wahlbewerber_mit_wahlkreis ohne Kandidatennummer daherkommt
+
+SELECT w."Kandidatennummer", COUNT(*) FROM raw.wahlbewerber_mit_wahlkreis ww, raw.wahlbewerber w
+WHERE 
+	ww."Vorname" = w."Vorname" AND
+	ww."Nachname" = w."Nachname" AND
+	ww."Jahrgang" = w."Jahrgang" AND
+	ww."Partei" = w."Partei"
+GROUP BY w."Kandidatennummer"
+HAVING COUNT(*) > 1
